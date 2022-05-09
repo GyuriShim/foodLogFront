b4fdce84033b47217fe8349fc0f9e4b00831735e
GyuriShim <rabitskr@naver.com>
2022-05-09 14:51:55 +0900

    chore: App 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import React from "react";
-import {View, StyleSheet, Text} from 'react-native';
+import React from "react"
+import {View, Text} from "react-native"
 
+const App = () => {
+	return(
+		<View>
+			<Text>Food Log</Text>
+		</View>
+	)
+}
 
-
-export default App;
+export default App
