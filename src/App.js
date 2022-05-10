9e611d42b09204e565de0f1b88b524e02e347e98
GyuriShim <rabitskr@naver.com>
2022-05-10 18:34:00 +0900

    feat: 웰컴 페이지 생성

--- src/App.js
+++ src/App.js
@@ -3,8 +3,23 @@ import {View, Text} from "react-native"
 
 const App = () => {
 	return(
-		<View>
-			<Text>Food Log</Text>
+		<View 
+			style={{
+				flex: 1,
+				backgroundColor: "rgba(190, 235, 255, 0.4)",
+				alignItems: "center",
+				justifyContent: "center",
+				
+			}}>
+			<Text
+				style={{
+					color: "#2f5d9a",
+					fontSize: 48,
+					fontWeight: "600",
+					paddingBottom: 200,
+				}}
+			>
+				Food{"\n"}Log</Text>
 		</View>
 	)
 }
