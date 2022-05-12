edbafecf8c5c5c4051e2649bb69bcb997b79d230
GyuriShim <rabitskr@naver.com>
2022-05-12 15:03:34 +0900

    feat: 로딩 화면 추가

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import {View, Text} from "react-native"
+import Loading from "./components/Loading"
 
 const App = () => {
-	return(
+	const [ready, setReady] = useState(true)
+	useEffect(()=>{
+		//뒤의 1000 숫자는 1초를 뜻함
+		//1초 뒤에 실행되는 코드들이 담겨 있는 함수     
+		setTimeout(()=>{         
+			//setState(data)
+			setReady(false) 
+		},2000)
+	},[]) 
+	return ready ? <Loading /> : (
 		<View 
 			style={{
 				flex: 1,
-				backgroundColor: "rgba(190, 235, 255, 0.4)",
+				backgroundColor: "#ffffff",
 				alignItems: "center",
 				justifyContent: "center",
-				
 			}}>
 			<Text
 				style={{
 					color: "#2f5d9a",
 					fontSize: 48,
 					fontWeight: "600",
-					paddingBottom: 200,
+					paddingBottom: 300,
 					fontFamily: "Multicolore Pro"
 				}}
 			>
