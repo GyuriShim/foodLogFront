7480d43d89bedc5e16ddc3558a265c830b1da97b
GyuriShim <rabitskr@naver.com>
2022-05-12 15:03:52 +0900

    add: 로딩화면 생성

---
+++ src/components/Loading.js
@@ -0,0 +1,28 @@
+import React from "react"
+import {View, Text} from "react-native"
+
+const Loading = () => {
+	return(
+		<View 
+			style={{
+				flex: 1,
+				backgroundColor: "rgba(190, 235, 255, 0.4)",
+				alignItems: "center",
+				justifyContent: "center",
+				
+			}}>
+			<Text
+				style={{
+					color: "#2f5d9a",
+					fontSize: 48,
+					fontWeight: "600",
+					paddingBottom: 200,
+					fontFamily: "Multicolore Pro"
+				}}
+			>
+				Food{"\n"}Log</Text>
+		</View>
+	)
+}
+
+export default Loading
