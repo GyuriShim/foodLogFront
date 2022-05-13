cabe298f251a53472e54f2d7f6f21f51139274ce
jiwon1031 <yewchung56@naver.com>
2022-05-13 16:12:37 +0900

    chore: 로그인 화면 수정

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,30 +1,50 @@
 import React from "react"
-import {View, Image, StyleSheet, StatusBar} from "react-native"
+import {View, Image, StyleSheet, StatusBar, Text} from "react-native"
 
 const Login = () => {
 	return(
 		<>
 			<StatusBar backgroundColor="#ffffff" barStyle="dark-content"/>
-			<View style={styles.view}>
+
+			
+			<View style={styles.view1}>
 				<Image 
 					source = {require("../assets/images/logo.png")}
 					style={styles.image}
 				/>
 			</View>
+			<View style={styles.view2}>
+				<Text style= {styles.text}>LOGIN</Text>
+			</View>
 		</>
 	)}
 
 const styles = StyleSheet.create({
-	view:{
-		flex: 1,
+	view1:{
+		flex: 0,
 		backgroundColor: "#ffffff",
-		alignItems: "center",
+		alignItems: "flex-start",
 		justifyContent: "center",
-		paddingBottom: 300
+		paddingLeft:30,
+		paddingTop: 50
+	},
+	view2:{
+		flex: 0,
+		backgroundColor: "#ffffff",
+		alignItems: "flex-start",
+		justifyContent: "center",
+		paddingLeft:30,
+		paddingTop: 50
 	},
 	image: {
-		height: 127,
-		width: 202,
+		height: 82,
+		width: 130,
+	},
+	text: {
+		fontSize: 24,
+		color: "#2F5D9A",
+		fontFamily: "Multicolore Pro",
+		
 	}
 })
 
