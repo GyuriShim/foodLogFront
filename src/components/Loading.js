eba3aaa74824d504953c2c50d7c4475119259421
GyuriShim <rabitskr@naver.com>
2022-05-13 15:06:32 +0900

    chore: 로딩 화면 스타일 수정

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,28 +1,32 @@
 import React from "react"
-import {View, Text} from "react-native"
+import {View, Image, StatusBar, StyleSheet} from "react-native"
 
 const Loading = () => {
 	return(
-		<View 
-			style={{
-				flex: 1,
-				backgroundColor: "rgba(190, 235, 255, 0.4)",
-				alignItems: "center",
-				justifyContent: "center",
-				
-			}}>
-			<Text
-				style={{
-					color: "#2f5d9a",
-					fontSize: 48,
-					fontWeight: "600",
-					paddingBottom: 200,
-					fontFamily: "Multicolore Pro"
-				}}
-			>
-				Food{"\n"}Log</Text>
-		</View>
+		<>
+			<StatusBar backgroundColor="rgba(190, 235, 255, 0.4)" barStyle="dark-content"/>
+			<View style={styles.view}>
+				<Image 
+					source = {require("../assets/images/logo.png")}
+					style={styles.image}
+				/>
+			</View>
+		</>
 	)
 }
+
+const styles = StyleSheet.create({
+	view:{
+		flex: 1,
+		backgroundColor: "rgba(190, 235, 255, 0.4)",
+		alignItems: "center",
+		justifyContent: "center",
+		paddingBottom: 300
+	},
+	image: {
+		height: 127,
+		width: 202,
+	}
+})
 
 export default Loading
