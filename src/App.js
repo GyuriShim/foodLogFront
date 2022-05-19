b4f69bccac039f4a057f8bb7771ec4d7b49026ea
GyuriShim <rabitskr@naver.com>
2022-05-19 18:59:40 +0900

    chore: 로딩화면 이후에 나오는 화면 수정

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, {useState, useEffect} from "react"
+import AddInfo from "./components/AddInfo"
 import Loading from "./components/Loading"
-import Login from "./components/Login"
+//import Login from "./components/Login"
 
 const App = () => {
 	const [ready, setReady] = useState(true)
 	useEffect(()=>{
 		//뒤의 1000 숫자는 1초를 뜻함
-		//1초 뒤에 실행되는 코드들이 담겨 있는 함수     
+		//2초 뒤에 실행되는 코드들이 담겨 있는 함수     
 		setTimeout(()=>{         
 			//setState(data)
 			setReady(false) 
 		},2000)
 	},[]) 
-	return ready ? <Loading /> : <Login />
+	return ready ? <Loading /> : <AddInfo />
 }
 
 export default App
