967e2c3ddda03f831da252354405ed21a5d8c430
GyuriShim <rabitskr@naver.com>
2022-05-22 21:26:55 +0900

    chore: 페이지 내용 수정

--- src/screens/Mapscreen.js
+++ src/screens/Mapscreen.js
@@ -1,20 +1,20 @@
 import React from "react"
-import { Button } from "react-native"
 import styled from "styled-components"
 
 const Container = styled.View`
     align-items: center
+	background-color: white
+	flex: 1
 `
 const StyledText = styled.Text`
     font-size: 30px
     margin-bottom: 10px
 `
-const Mapscreen = () => {
+const MapScreen = () => {
 	return(
 		<Container>
 			<StyledText>mapscreen</StyledText>
-			<Button title="hello anyang" style={{height: 20, width: 50}}/>
 		</Container>
 	)
 }
-export default Mapscreen
+export default MapScreen
