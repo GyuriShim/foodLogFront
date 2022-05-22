d5a2d2c6a19bd505e0ab2550f06657050e669634
GyuriShim <rabitskr@naver.com>
2022-05-22 21:22:36 +0900

    add: 아이콘 종류별 컴포넌트 생성

---
+++ src/assets/icons/Location.js
@@ -0,0 +1,6 @@
+import React from "react"
+import Icon from "react-native-vector-icons/Ionicons"
+
+export const Location = ({name, size, color}) => {
+	return <Icon name={name} size={size} color={color}/>
+}
