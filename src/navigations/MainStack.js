90ad6087f2907868c1868e2df7359e571b3a6a4b
GyuriShim <rabitskr@naver.com>
2022-05-31 15:45:15 +0900

    feat: 검색창 스택 스크린에 추가

--- src/navigations/MainStack.js
+++ src/navigations/MainStack.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { createStackNavigator } from "@react-navigation/stack"
 import MainTab from "./MainTab"
+import SearchScreen from "../screens/SearchScreen"
+import SearchHeader from "../components/SearchHeader"
 
 
 const Stack = createStackNavigator()
@@ -8,6 +10,13 @@ const MainStack = () => {
 	return (
 		<Stack.Navigator>
 			<Stack.Screen name="main" component={MainTab} options={{headerShown: false}}/>
+			<Stack.Screen 
+				name="search" 
+				component={SearchScreen} 
+				options={{
+					title: "검색",
+					headerTitle: () => <SearchHeader />
+				}}/>
 		</Stack.Navigator>
 	)
 }
