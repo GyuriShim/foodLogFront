13dc34e0efaef62845caf72dc8d96267bbaf0387
GyuriShim <rabitskr@naver.com>
2022-05-31 15:35:33 +0900

    add: Fontisto 아이콘 컴포넌트 생성

---
+++ src/assets/icons/Fontisto.js
@@ -0,0 +1,7 @@
+import React from "react"
+import Icon from "react-native-vector-icons/Fontisto"
+
+export const FontIcon = ({name, size, color}) => {
+	return <Icon name={name} size={size} color={color}/>
+}
+
