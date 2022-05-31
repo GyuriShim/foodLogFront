702565d3c1288d61355e256f922b6162d1e1ea86
GyuriShim <rabitskr@naver.com>
2022-05-31 15:38:34 +0900

    chore: 지도 화면 확인을 위한 로그인 초기값 수정

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,14 +1,17 @@
 import { NavigationContainer } from "@react-navigation/native"
 import React, {useState} from "react"
+import { SearchContextProvider } from "../contexts/SearchContext"
 import AuthStack from "./AuthStack"
 import MainStack from "./MainStack"
 
 const Navigation = () => {
-	const [login, setLogin] = useState(false)
+	const [login, setLogin] = useState(true)
 
 	return(
 		<NavigationContainer>
-			{login ? <MainStack /> : <AuthStack />}
+			<SearchContextProvider>
+				{login ? <MainStack /> : <AuthStack />}
+			</SearchContextProvider>
 		</NavigationContainer>
 	)
 }
