fc675db0909abe415c8f9d943b8753cecbe2adef
jiwon1031 <yewchung56@naver.com>
2022-06-07 19:07:06 +0900

    feat: 업로드스크린설정

--- src/navigations/MainStack.js
+++ src/navigations/MainStack.js
@@ -1,13 +1,16 @@
 import React from "react"
 import { createStackNavigator } from "@react-navigation/stack"
 import MainTab from "./MainTab"
-
+import UploadScreen from "../screens/UploadScreen"
 
 const Stack = createStackNavigator()
 const MainStack = () => {
 	return (
 		<Stack.Navigator>
 			<Stack.Screen name="main" component={MainTab} options={{headerShown: false}}/>
+			<Stack.Screen name="upload" component={UploadScreen}
+				options={{title: "new post", headerBackTitle: "back"}}
+			/>
 		</Stack.Navigator>
 	)
 }
