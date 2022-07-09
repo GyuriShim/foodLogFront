c8e63f123ea94c74b122cfbd0e04ad60f82c0826
GyuriShim <rabitskr@naver.com>
2022-07-09 23:07:07 +0900

    chore: 검색화면 헤더 스타일 추가

--- src/navigations/MainStack.js
+++ src/navigations/MainStack.js
@@ -8,14 +8,24 @@ import SearchHeader from "../components/SearchHeader"
 const Stack = createStackNavigator()
 const MainStack = () => {
 	return (
-		<Stack.Navigator>
+		<Stack.Navigator
+			screenOptions={{
+				cardStyle: {backgroundColor: "white"}
+			}}
+		>
 			<Stack.Screen name="main" component={MainTab} options={{headerShown: false}}/>
 			<Stack.Screen 
 				name="search" 
 				component={SearchScreen} 
 				options={{
 					title: "검색",
-					headerTitle: () => <SearchHeader />
+					headerTitle: () => <SearchHeader />,
+					headerStyle: {
+						height:100, 
+						backgroundColor: "rgba(190, 235, 255, 0.4)", 
+						borderBottomColor: "#ccc", 
+						borderBottomWidth:2
+					},
 				}}/>
 		</Stack.Navigator>
 	)
