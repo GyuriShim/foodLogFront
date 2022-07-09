3d2ddb964bf4c1a98d332828256d5898656d876b
GyuriShim <rabitskr@naver.com>
2022-07-09 23:05:31 +0900

    feat: 개인 지도 추가

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,11 +1,13 @@
 import React from "react"
 import { Button } from "react-native"
 import styled from "styled-components"
+import MapView from "react-native-maps"
 
 const Container = styled.View`
-    align-items: center
-    background-color: white
+	align-items: center
+	background-color: white
 	flex: 1
+	padding: 15px
 `
 const StyledText = styled.Text`
     font-size: 30px
@@ -15,10 +17,37 @@ const AccountScreen = () => {
 	return(
 		<>
 			<Container>
-				<StyledText>내 계정</StyledText>
-				<Button title="hello anyang" style={{height: 20, width: 50}}/>
+				<MapView
+					style={{height: "85%", width: "100%", borderRadius: 90}}
+					initialRegion={{
+						latitude: 35.95, 
+						longitude: 127.5,
+						latitudeDelta: 5,
+						longitudeDelta: 5
+					}}
+					customMapStyle={mapStyle}
+				>
+				</MapView>
 			</Container>
 		</>
 	)
 }
 export default AccountScreen
+
+const mapStyle = [
+	{
+		"stylers": [
+			{
+				"lightness": 15
+			}
+		]
+	},
+	{
+		"featureType": "road",
+		"stylers": [
+			{
+				"visibility": "simplified"
+			}
+		]
+	}
+]
