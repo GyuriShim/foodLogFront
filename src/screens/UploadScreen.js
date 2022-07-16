238b39cfa5d7d34dbab21adf4fff0b30910bdd20
jiwon1031 <yewchung56@naver.com>
2022-07-16 15:10:33 +0900

    feat: 게시글 업로드 화면

--- src/screens/UploadScreen.js
+++ src/screens/UploadScreen.js
@@ -1,14 +1,15 @@
-import React,{useState} from "react"
-import { Button } from "react-native"
+import React,{useState, useEffect, useRef} from "react"
 import styled from "styled-components"
 import ImagePicker from "react-native-image-picker"
 import { launchImageLibrary } from "react-native-image-picker"
-import {Image, StatusBar, View, Text, StyleSheet, TouchableOpacity, Platform, Alert} from "react-native"
+import {Image, StatusBar, View, Text, StyleSheet, TouchableOpacity, Platform, Alert,Button, TextInput, useWindowDimensions} from "react-native"
 import Pressable from "react-native/Libraries/Components/Pressable/Pressable"
 import NativeImagePickerIOS from "react-native/Libraries/Image/NativeImagePickerIOS"
 import ImagePick from "../components/ImagePick"
+import {useRoute} from "@react-navigation/native"
 
-const Container = styled.View`
+
+/*const Container = styled.View`
     align-items: center
     background-color: white
 	flex: 1
@@ -16,10 +17,39 @@ const Container = styled.View`
 const StyledText = styled.Text`
     font-size: 30px
     margin-bottom: 10px
-` 
+` */
 const UploadScreen = () => {
+	const route = useRoute()
+	const {res} = route.params || {}
+	const {width} = useWindowDimensions()
+
 	return(
-		<ImagePick/>
+		<View style={styles.block}>
+			<Image
+				source={{uri: res.assets[0]?.uri}}
+				style={[styles.image, {height: width}]}
+				resizeMode="cover"
+			/>
+			<TextInput
+				style={styles.input}
+				multiline={true}
+				placeholder="게시글을 입력하세요"
+				textAlignVertical="top"
+			/>
+		</View>
 	)
 }
+const styles = StyleSheet.create({
+	block: {
+		flex: 1
+	},
+	image: {width: "100%"},
+	input: {
+		paddingHorizontal: 16,
+		paddingTop: 16,
+		paddingBottom: 16,
+		flex: 1,
+		fontSize: 16,
+	},
+})
 export default UploadScreen
