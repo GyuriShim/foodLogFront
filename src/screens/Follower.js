26c340c010476eb1340e35086fcc2b916ad733d4
GyuriShim <rabitskr@naver.com>
2022-09-28 23:26:32 +0900

    add: 팔로잉 팔로워 화면 생성

---
+++ src/screens/Follower.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { ScrollView, Text } from "react-native"
+
+const Follower = () => {
+	return (
+		<ScrollView>
+			<Text></Text>
+		</ScrollView>
+	)
+}
+
+export default Follower
