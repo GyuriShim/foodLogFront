f7856624533347cb5a60995a073e2f904cc12b1e
jiwon1031 <yewchung56@naver.com>
2022-09-28 15:29:00 +0900

    add: 게시글화면추가

--- src/navigations/MainStack.js
+++ src/navigations/MainStack.js
@@ -2,16 +2,25 @@ import React from "react"
 import { createStackNavigator } from "@react-navigation/stack"
 import MainTab from "./MainTab"
 import UploadScreen from "../screens/UploadScreen"
+import PostScreen from "../screens/PostScreen"
 
 const Stack = createStackNavigator()
 const MainStack = () => {
 	return (
-		<Stack.Navigator>
+		
+		<Stack.Navigator
+			screenOptions={{
+				cardStyle: {backgroundColor: "white"}
+			}}
+		>
 			<Stack.Screen name="main" component={MainTab} options={{headerShown: false}}/>
 			<Stack.Screen name="upload" component={UploadScreen}
-				options={{title: "new post", headerBackTitle: "back"}}
+				//options={{title: "new post", headerBackTitle: "back"}}
 			/>
+			<Stack.Screen name="PostScreen" component={PostScreen}
+				options={{title: "게시물", headerBackTitle: "back"}}/>
 		</Stack.Navigator>
+		
 	)
 }
 export default MainStack
---
+++ src/screens/PostScreen.js
@@ -0,0 +1,154 @@
+import React, {useState} from "react"
+import {View, Text, Pressable, StyleSheet, Image, ScrollView, TouchableOpacity,KeyboardAvoidingView, Platform}from "react-native"
+import { TextInput } from "react-native-gesture-handler"
+import {Location} from "../assets/icons/Location"
+//import {CustomRatingBar} from "../screens/UploadScreen"
+import styled from "styled-components"
+import Button from "../components/Button"
+
+
+
+const Box1 = styled.View`
+  flex: 1
+  border-radius: 10px
+  margin: 3px 40px
+  background-color: white
+  border: 4px rgba(164, 212, 234, 0.6)
+  align-items: flex-start
+`
+
+const styles = StyleSheet.create({
+	profile: {
+		width: 40,
+		height: 40,
+		borderRadius: 50,
+		backgroundColor: "black",
+		alignItems: "center",
+		marginRight: 5
+	},
+	customRatingBarStyle : {
+		justifyContent: "flex-end",
+		flexDirection: "row",
+		marginTop: 3,
+	},
+	starImgStyle : {
+		padding: 15,
+		width: 10,
+		height: 10,
+		resizeMode: "cover"
+	},
+	button: {
+		alignItems: "center",
+		width: 45,
+		height: 35,
+		justifyContent: "center",
+		//marginHorizontal: 10,
+		borderRadius: 7
+	},
+	block:{
+		height: 50,
+		paddingHorizontal: 16,
+		borderColor: "#C9EEFF",
+		borderTopWidth: 2,
+		borderBottomWidth: 2,
+		alignContent: "center",
+		flexDirection: "row",
+		//justifyContent: "center",
+	},
+	input: {
+		flex : 1,
+		fontSize: 10,
+		paddingVertical: 10,
+	},
+	avoid:{
+		flex:1,
+	},
+})
+
+function PostScreen(){
+	const [text, setText] = useState("")
+	const [defaultRating, setdefaultRating] =useState(5)
+	const [maxRating, setMaxRating] = useState([1,2,3,4,5])
+	const starImgFilled =  "https://raw.githubusercontent.com/tranhonghan/images/main/star_filled.png"
+	const starImgCorner = "https://raw.githubusercontent.com/tranhonghan/images/main/star_corner.png"
+
+	const CustomRatingBar = () => {
+		return (
+			<View style={styles.customRatingBarStyle}>
+				{
+					maxRating.map((item, key)=>{
+						return (
+							<View
+								activeOpacity={0.7}
+								key= {item}
+								onPress = {()=>setdefaultRating(item)}
+							>
+								<Image
+									style={styles.starImgStyle} 
+									source={
+										item <= defaultRating
+											? {uri: starImgFilled}
+											: {uri: starImgCorner}
+									}
+								/>
+							</View>
+						)						
+					})
+				}
+			</View>
+		)
+	}
+
+	return(
+
+		<ScrollView>
+			<KeyboardAvoidingView 
+				behavior={Platform.OS === "android" ? "padding" : undefined}
+				style={styles.avoid}>
+				<View style={{width: "100%", /* backgroundColor: "rgba(165, 212, 233, 0.3)", */ marginBottom: 15, paddingBottom: 5, paddingHorizontal: 5}}>
+					<View style={{flexDirection: "row", alignItems: "center", justifyContent: "space-between", paddingVertical: 5}}>   
+						<Pressable>
+							<View style={{flexDirection: "row", alignItems: "center"}}>
+								<Image style={styles.profile}/>
+								<Text>dfjlkwjk</Text> 
+							</View>
+						</Pressable>
+						<Text>2022.09.21</Text>
+					</View>
+					<Image style={{width: "100%", height: 350, backgroundColor: "white", marginBottom: 5}}/>
+					<View style ={{flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}>
+						<Text style={{fontSize: 16}}>엄지네 포장마차</Text>
+						<CustomRatingBar/> 
+					</View>
+					<View style={{flexDirection: "row", alignItems: "center", marginBottom: 10}}>
+						<Location name="location-outline" size={14}/>
+						<Text>강원 강릉시 남구길30번길 22</Text>
+					</View>
+					<Text>
+                아주 맛있었다. 강릉에 또 간다면 무조건 들를 맛집이다. 그리고 너무 행복했던 여행
+					</Text>
+				</View>
+			
+				<View style = {styles.block}>
+					{/* style ={{flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}  */}
+					<TextInput
+						style = {styles.input}
+						//multiline = {true}
+						placeholder = "댓글을 입력하세요"
+						value={text}
+						onChangeText={setText}
+					//textAlignVertical="center"
+					/>
+					<TouchableOpacity style = {{flexDirection: "row", alignItems: "center", justifyContent: "space-between"}} activeOpacity={0.5}>
+						<Button title="등록" onPress={() => {}}/>
+						{/* <View style = {styles.button}>
+						</View> */}
+					</TouchableOpacity>
+				</View>
+			</KeyboardAvoidingView>
+		</ScrollView>
+	)
+}
+
+
+export default PostScreen
