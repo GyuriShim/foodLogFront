577ccf65a4aab913de93903a470b62fa62dec5ed
YuHayeon <youha0930@gmail.com>
2022-10-03 15:04:37 +0900

    fix: navigator merge

--- src/navigations/MainStack.js
+++ src/navigations/MainStack.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { createStackNavigator } from "@react-navigation/stack"
 import MainTab from "./MainTab"
+import UploadScreen from "../screens/UploadScreen"
+import PostScreen from "../screens/PostScreen"
 import SearchScreen from "../screens/SearchScreen"
 import SearchHeader from "../components/SearchHeader"
 import Follower from "../screens/Follower"
@@ -12,12 +14,13 @@ import AccountScreen from "../screens/AccountScreen"
 const Stack = createStackNavigator()
 const MainStack = () => {
 	return (
+		
 		<Stack.Navigator
 			screenOptions={{
 				cardStyle: {backgroundColor: "white"}
 			}}
 		>
-			<Stack.Screen name="main" component={MainTab} options={{headerShown: false}}/>
+			<Stack.Screen name="main" component={MainTab} options={{ headerShown: false }} />
 			<Stack.Screen 
 				name="search" 
 				component={SearchScreen} 
@@ -69,6 +72,11 @@ const MainStack = () => {
 				component={AccountScreen}
 				options={{title: "계정 아이디"}}
 			/>
+			<Stack.Screen name="upload" component={UploadScreen}
+				//options={{title: "new post", headerBackTitle: "back"}}
+			/>
+			<Stack.Screen name="PostScreen" component={PostScreen}
+				options={{title: "게시물", headerBackTitle: "back"}}/>
 		</Stack.Navigator>
 		
 	)
--- src/navigations/MainTab.js
+++ src/navigations/MainTab.js
@@ -17,35 +17,21 @@ const MainTab = () => {
 		<>
 			<StatusBar backgroundColor="white" barStyle="dark-content"/>
 			<Tab.Navigator
+				tabBarOptions={{inactiveTintColor:"black"}}
 				screenOptions={{
-					tabBarHideOnKeyboard: true,
-					tabBarInactiveTintColor: "black",
-					tabBarStyle:[
-						{
-							backgroundColor: "rgb(216, 243, 255)",
-							borderTopColor: "rgb(216, 243, 255)",
-							borderBottomColor: "rgb(216, 243, 255)",
-							borderTopWidth: 2,
-							borderBottomWidth: 2,
-							height: 58
-						},
-						null
-					]
+					tabBarStyle:{
+						backgroundColor: "rgb(216, 243, 255)",
+						borderTopColor: "rgb(216, 243, 255)",
+						borderBottomColor: "rgb(216, 243, 255)",
+						borderTopWidth: 2,
+						borderBottomWidth: 2,
+					}
 				}}
 			>	
 				<Tab.Screen name="지도" component={MapScreen} options={{headerShown:false, tabBarIcon: props => Location({...props, name:"location-outline", size: 24})}}/>
 				<Tab.Screen name="추천" component={RcmdScreen} options={{tabBarIcon: props => AntIcon({...props, name:"like2"})}}/>
-				<Tab.Screen name="업로드" component={UploadScreen} options={{tabBarIcon: props => AntIcon({...props, name:"pluscircleo"})}}/>
-				<Tab.Screen 
-					name="구독" 
-					component={SubScreen} 
-					options={{
-						tabBarIcon: props => OcticonsIcon({...props, name:"people"}), 
-						title: "구독자 게시물", 
-						headerTitleStyle: {
-							fontSize: 16
-						}
-					}}/>
+				<Tab.Screen name="게시물 등록" component={UploadScreen} options={{tabBarIcon: props => AntIcon({...props, name:"pluscircleo"})}}/>
+				<Tab.Screen name="구독" component={SubScreen} options={{tabBarIcon: props => OcticonsIcon({...props, name:"people"})}}/>
 				<Tab.Screen name="내 계정" component={AccountScreen} options={{headerShown:false, tabBarIcon: props => OcticonsIcon({...props, name:"person"})}}/>
 			</Tab.Navigator>
 		</>
