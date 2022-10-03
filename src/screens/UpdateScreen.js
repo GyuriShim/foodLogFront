139878a9887eca451097d11df03c3ea0d5228d9b
jiwon1031 <yewchung56@naver.com>
2022-10-03 14:47:35 +0900

    add:게시글수정파일생성

---
+++ src/screens/UpdateScreen.js
@@ -0,0 +1,15 @@
+import React,{useState} from "react"
+import { launchImageLibrary } from "react-native-image-picker"
+import {ScrollView, Image, View, Text,  TouchableOpacity, Platform, TextInput, ActivityIndicator, StyleSheet} from "react-native"
+import axios from "axios"
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view"
+mport styled from "styled-components"
+import Button from "../components/Button"
+import DateTimePickerModal from "react-native-modal-datetime-picker"
+import RNPickerSelect from "react-native-picker-select"
+
+function UpdateScreen() {
+    
+}
+
+export default UpdateScreen
