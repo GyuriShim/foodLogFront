b9e7a8a0c25ea9eb915cca800ec3e780916de380
YuHayeon <youha0930@gmail.com>
2022-10-07 17:30:26 +0900

    feat: join - AsyncStorage 수정, formData 진행중

--- src/utils/StorageFun.js
+++ src/utils/StorageFun.js
@@ -1,25 +1,72 @@
 import AsyncStorage from "@react-native-async-storage/async-storage"
 
-export const getItemFromAsync = async(storageName) => {
-	try {
-		await AsyncStorage.getItem(storageName, (err, result) => {
-			const data = JSON.parse(result)
-			console.log(data)
-		})
-	} catch (error) {
-		console.log("불러오기 실패")
+const isEmpty = function (value) {
+	if (value === "" || value === null || value === undefined || (value !== null && typeof value === "object" && !Object.keys(value).length)) {
+		return true
+	} else {
+		return false
 	}
 }
-
-export const setItemToAsync = async(storageName, item) => {
-	try{
-		await AsyncStorage.setItem(storageName, JSON.stringify(item))
-		console.log("save successfully")
-	} catch (e){
-		console.log("save error : Asynce Storage")
+  
+// AsyncStorage get 함수 모듈
+export const getItemFromAsync = (storageName) => {
+	if (isEmpty(storageName)) {
+		throw Error("Storage Name is empty")
 	}
 	
+	return new Promise((resolve, reject) => {
+		AsyncStorage.getItem(storageName, (err, result) => {
+			if (err) {
+				reject(err)
+			}
+		
+			if (result === null) {
+				resolve(null)
+			}
+			console.log(result)
+			resolve(JSON.parse(result))
+			
+		})
+	})
 }
+  
+// AsyncStorage set 함수 모듈
+export const setItemToAsync = (storageName, item) => {
+	if (isEmpty(storageName)) {
+		throw Error("Storage Name is empty")
+	}
+  
+	return new Promise((resolve, reject) => {
+		AsyncStorage.setItem(storageName, JSON.stringify(item), (error) => {
+			if (error) {
+				reject(error)
+			}
+
+			resolve("입력 성공")
+		})
+	})
+}
+
+// export const getItemFromAsync = async(storageName) => {
+// 	try {
+// 		await AsyncStorage.getItem(storageName, (err, result) => {
+// 			const data = JSON.parse(result)
+// 			console.log(data)
+// 		})
+// 	} catch (error) {
+// 		console.log("불러오기 실패")
+// 	}
+// }
+
+// export const setItemToAsync = async(storageName, item) => {
+// 	try{
+// 		await AsyncStorage.setItem(storageName, JSON.stringify(item))
+// 		console.log("save successfully")
+// 	} catch (e){
+// 		console.log("save error : Asynce Storage")
+// 	}
+	
+// }
 
 export const clearAll = async() => {
 	try {
