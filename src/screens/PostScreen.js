7e7f175c651fc22fcb5644982b6b73868da265bb
YuHayeon <youha0930@gmail.com>
2022-10-10 15:59:14 +0900

    feat: 게시물 조회 API 연동

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,4 +1,5 @@
-import React, {useState} from "react"
+import React, { useEffect, useState } from "react"
+import axios from "axios"
 import {View, Text, Pressable, StyleSheet, Image, ScrollView, TouchableOpacity,KeyboardAvoidingView, Platform}from "react-native"
 import { TextInput } from "react-native-gesture-handler"
 import {Location} from "../assets/icons/Location"
@@ -7,8 +8,6 @@ import styled from "styled-components"
 import Button from "../components/Button"
 import { format, formatDistanceToNow } from "date-fns"
 import {ko} from "date-fns/locale"
-
-
 
 const Box1 = styled.View`
   flex: 1
@@ -73,6 +72,57 @@ function PostScreen(date){
 	const [maxRating, setMaxRating] = useState([1,2,3,4,5])
 	const starImgFilled =  "https://raw.githubusercontent.com/tranhonghan/images/main/star_filled.png"
 	const starImgCorner = "https://raw.githubusercontent.com/tranhonghan/images/main/star_corner.png"
+	const [post, setPost] = useState({})
+	const [place, setPlace] = useState({})
+	const [pictures, setPictures] = useState([])
+	const [comment, setComment] = useState([])
+	const [loading, setLoading] = useState(false)
+	const [error, setError] = useState(null)
+
+	const fetchPost = async () => {
+		try {
+			// 요청이 시작 할 때에는 error 와 users 를 초기화하고
+			setError(null)
+			// loading 상태를 true 로 바꿉니다.
+			setLoading(true)
+			const response = await axios.get("http://food-log-dku.com:8080/api/v1/post/37", {
+				headers: {
+					"ACCESS-TOKEN" : "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0MkBnbWFpbC5jb20iLCJpc3MiOiJmb29kIGxvZyIsIm1lbWJlcklkIjoxMSwiaWF0IjoxNjY1Mzc4MTEzLCJleHAiOjE2NjUzODg5MTN9.Jsjr8GXurCMk6Y3MfIKJC9uMGRNnubWNgmnm7Fw8iZkwdhJeQFFB8zHJ7hObedQuI6FRPP9oRgZb1qFGhoOKfQ"
+				}
+			})
+			setPost(response.data)
+			setPlace(response.data.place)
+			setComment(response.data.comment)
+			setPictures(response.data.pictures)
+			// .then((res) => {
+			// 	if (res.status === 200) {
+			// 		setPost(res.data)
+			// 		console.log("hihihihi",res.data.member)
+			// 	} else {
+			// 		console.log(res)
+			// 	}
+			// })
+			// .catch((error) => {
+			// 	console.log("get Post error", error)
+			// })
+
+		} catch (e) {
+			setError(e)
+			console.log("catch error", e)
+		}
+
+		setLoading(false)
+	}
+
+	useEffect(() => {
+		fetchPost()
+	}, [])
+
+	console.log("data : ", post)
+	console.log("pictures" , pictures)
+	// console.log("Picture", post.pictures[0])
+
+	if (loading) return <Text>로딩 중</Text>
 
 	const CustomRatingBar = () => {
 		return (
@@ -125,22 +175,24 @@ function PostScreen(date){
 						<Pressable>
 							<View style={{flexDirection: "row", alignItems: "center"}}>
 								<Image style={styles.profile}/>
-								<Text>dfjlkwjk</Text> 
+								<Text>{post.member}</Text> 
 							</View>
 						</Pressable>
 						<Text>{/* formatDate(date) */}</Text>
 					</View>
-					<Image style={{width: "100%", height: 350, backgroundColor: "white", marginBottom: 5}}/>
+					<Image style={{ width: "100%", height: 350, backgroundColor: "white", marginBottom: 5 }}
+						source={{ uri: pictures[0] }}
+					/>
 					<View style ={{flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}>
-						<Text style={{fontSize: 16}}>엄지네 포장마차</Text>
+						<Text style={{fontSize: 16}}>{place.name}</Text>
 						<CustomRatingBar/> 
 					</View>
 					<View style={{flexDirection: "row", alignItems: "center", marginBottom: 10}}>
 						<Location name="location-outline" size={14}/>
-						<Text>강원 강릉시 남구길30번길 22</Text>
+						<Text>{place.address}</Text>
 					</View>
 					<Text>
-                아주 맛있었다. 강릉에 또 간다면 무조건 들를 맛집이다. 그리고 너무 행복했던 여행
+						{post.review}
 					</Text>
 				</View>
 			
