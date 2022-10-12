7c8488cfd7cfe78bcc2dbf4f8f87808148b266ea
jiwon1031 <yewchung56@naver.com>
2022-10-12 15:44:54 +0900

    feat: 이미지날짜추출

--- src/screens/UploadScreen.js
+++ src/screens/UploadScreen.js
@@ -1,6 +1,6 @@
 import React,{useState, useEffect, useRef} from "react"
 import { launchImageLibrary } from "react-native-image-picker"
-import {ScrollView, Image, StatusBar, View, Text,  TouchableOpacity, Platform, TextInput, ActivityIndicator, StyleSheet, Alert} from "react-native"
+import {ScrollView, Image, StatusBar, View, Text,  TouchableOpacity, Platform, TextInput, ActivityIndicator, StyleSheet, Alert, ImageBackground} from "react-native"
 import Pressable from "react-native/Libraries/Components/Pressable/Pressable"
 import {useRoute} from "@react-navigation/native"
 import axios from "axios"
@@ -9,6 +9,8 @@ import styled from "styled-components"
 import Button from "../components/Button"
 import DateTimePickerModal from "react-native-modal-datetime-picker"
 import RNPickerSelect from "react-native-picker-select"
+import ImagePicker from "react-native-image-picker"
+import Exif from "react-native-exif"
 
 
 const Container = styled.View` 
@@ -91,29 +93,32 @@ const styles = StyleSheet.create({
 		backgroundColor: "white",
 		alignContent: "center",
 		alignItems: "center",
+		paddingLeft: 10,
 	},
 })
 function UploadScreen({onChangeDate, navigation }){
 	const [defaultRating, setdefaultRating] =useState(2)
 	const [maxRating, setMaxRating] = useState([1,2,3,4,5])
 	const [loading, setLoading] = useState(false)
-	//const [date, setDate] = useState( log ? new Date(log.date) : new Date())
+	const [date, setDate] = useState("")
 	const [mode, setMode] = useState("date")
 	const [visible, setVisible] = useState(false)
 	const [show, setShow] = useState(false)
 	const [text, setText] = useState("Empty")
 	const placeholder = "목적을 입력해주세요."
 	const [response, setResponse] = useState(null)
+	const [image, setImage] = useState("")
 	
 	const onPressDate = () => {
 		setMode("date")
 		setVisible(true)
+
 	}
 
-	const onConfirm = (selectedDate) => {
+	const onConfirm = (date) => {
 		setVisible(true)
-		onChangeDate(selectedDate)
-		console.log(onChangeDate)
+		onChangeDate(date)
+		console.log(date)
 	}
 
 	const onCancel = () => {
@@ -153,8 +158,28 @@ function UploadScreen({onChangeDate, navigation }){
 			</View>
 		)
 	}
+
+	/*const selectImage = async() =>{
+		
+		ImagePicker.openPicker({
+			width: 300,
+			height: 400,
+			cropping: true ,
+			includeExif: true,
+		}).then(image => {
+			console.log(image.modificationDate)
+			console.log(image.exif)
+			setImage(image.path)
+			const date = new Date(image.modificationDate)
+			date.setDate(date.getDate() - 1)
+			let month = `${date.getMonth() + 1}`
+			let day = `${date.getDate()}`
+		})
+		
+	}*/
 	
 	const selectImage = async() => {
+		//var ImagePicker = require("react-native-image-picker")
 		const image = {
 			uri: "",
 			type: "",
@@ -165,8 +190,18 @@ function UploadScreen({onChangeDate, navigation }){
 			{
 				mediaType: "photo",
 				includeBase64: Platform.OS === "android",
+				includeExtra: true,
 			},
-			(res) =>{
+			(response) => {
+				setResponse(response)
+				var originDate = response.assets[0].timestamp.split("T")
+				setDate(originDate[0])
+				
+				console.log("response: ", response.assets[0].timestamp)
+				console.log("response latitude: ", response.latitude)
+				console.log("response longitude: ", response.longitude)
+			}
+			/* (res) =>{
 				setResponse(res)
 				if(res.didCancel){
 					console.log("User cancelled image picker")
@@ -179,46 +214,48 @@ function UploadScreen({onChangeDate, navigation }){
 					image.name = res.assets[0].fileName
 					image.type = res.assets[0].type
 					image.uri = Platform.OS === "android" ? res.assets[0].uri : res.assets[0].uri.replace("file://", "")
-					image.timestamp = res.assets[0].timestamp
-					//console.log(image.timestamp)
+					var assets0 = res.assets[0].timestamp
+					console.log(res.assets)
+					//const source = {uri: response.uri}
 				}
 				
-			}
+			} */
 		)
-		const formData = new FormData()
-		formData.append("multipartFile", image)
-		//alert(res.assets[0].uri)
-		const headers = {
-			"Content-Type" : "multipart/form-data; boundary=someArbitraryUniqueString",
-		}
-		console.log(image)
-		console.log(formData)
+	}
+	/* const formData = new FormData()
+	formData.append("multipartFile", image)
+	//alert(res.assets[0].uri)
+	const headers = {
+		"Content-Type" : "multipart/form-data; boundary=someArbitraryUniqueString",
+	}
+	console.log(image)
+	console.log(formData)
 
-		axios.post("https://localhost:8080/post", formData, {headers: headers})
-			.then(response => {
-				if(response){
-					console.log("rrrr", response.data)
-				}
-			})
-			.catch((error)=> {
-				if (error.res) {
-					console.log("errormessage", error)
-					// The request was made and the server responded with a status code
-					// that falls out of the range of 2xx
-					console.log(error.response.data)
-					console.log(error.response.status)
-					console.log(error.response.headers)
-				} else if (error.request) {
+	axios.post("https://localhost:8080/post", formData, {headers: headers})
+		.then(response => {
+			if(response){
+				console.log("rrrr", response.data)
+			}
+		})
+		.catch((error)=> {
+			if (error.res) {
+				console.log("errormessage", error)
+				// The request was made and the server responded with a status code
+				// that falls out of the range of 2xx
+				console.log(error.response.data)
+				console.log(error.response.status)
+				console.log(error.response.headers)
+			} else if (error.request) {
 				// The request was made but no response was received
 				// `error.request` is an instance of XMLHttpRequest in the browser and an instance of
 				// http.ClientRequest in node.js
-					console.log(error.request)
-				} else {
+				console.log(error.request)
+			} else {
 				// Something happened in setting up the request that triggered an Error
-					console.log("Error", error.message)
-				}
-			})
-	}
+				console.log("Error", error.message)
+			}
+		}) */
+	
 	return (
 		//<SafeAreaView>
 		<KeyboardAwareScrollView contentContainerStyle={{flex:1}}>
@@ -227,15 +264,18 @@ function UploadScreen({onChangeDate, navigation }){
 					<Box1>
 						<View style={{flex:0, padding:1}}>
 							<Button title="이미지 업로드" color={"lightblue"} onPress={selectImage}></Button>
-							<Image 
+							<ImageBackground
 								style={{width: 200, height:200, justifyContent: "center", alignItems: "center"}}
-								source={{uri: response?.assets[0]?.uri}}
+								source={{uri: response?.assets[0]?.uri}}// }}
 								resizeMode="cover"
 							/>
 						</View>
 					</Box1>
 					<View style = {styles.Box2}>
-						<Text style={{flex: 1}}>{"날짜"}</Text>
+						{response ? 
+							<Text style={{flex: 1}}>{date}</Text>:
+							<Text style={{flex: 1}}>{"날짜"}</Text>}
+						
 						<TouchableOpacity style = {{flexDirection: "row", alignItems: "center", justifyContent: "space-between"}} activeOpacity={0.5}>
 							<Button title="등록" onPress={onPressDate}/>
 						</TouchableOpacity>
@@ -299,12 +339,11 @@ function UploadScreen({onChangeDate, navigation }){
 					{loading ? (
 						<ActivityIndicator style={styles.spinner} />
 					) :  (
-						<form onSubmit= {selectImage} entype="multipart/form-data">
+						<Button title="다음" color={"rgba(165, 212, 233, 0.5)"} containerStyle={styles.button} onPress={() => navigation.navigate("PostScreen")}/>
+					)}
+					{/* <form onSubmit= {selectImage} entype="multipart/form-data">
 							<input type="file"/>
-							<Button type="submit" title="다음" color={"rgba(165, 212, 233, 0.5)"} containerStyle={styles.button} onPress={() => navigation.navigate("PostScreen")}/>
-						</form>)
-					}
-					
+						</form>*/}
 				</View>
 			</Container> 
 		</KeyboardAwareScrollView>
