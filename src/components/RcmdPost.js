d8c0a72d919998b69ace9024e25b8d240b0661a8
GyuriShim <rabitskr@naver.com>
2022-10-14 19:44:45 +0900

    feat: 별점 및 변수 추가

--- src/components/RcmdPost.js
+++ src/components/RcmdPost.js
@@ -1,35 +1,80 @@
-import React from "react"
-import { Dimensions, View, Image, Text } from "react-native"
+import React, {useState} from "react"
+import { Dimensions, View, Image, Text, Pressable, StyleSheet } from "react-native"
 import { Location } from "../assets/icons/Location"
 
-const RcmdPost = () => {
+function RcmdPost({item:{imageUrl, store, address, contents, rating}, onPress}){
 	const windowHeight = Dimensions.get("window").height
 	const windowWidth = Dimensions.get("window").width
 
+	const CustomRatingBar = () => {
+		const defaultRating = rating
+		const [maxRating, setMaxRating] = useState([1,2,3,4,5])
+		const starImgFilled =  "https://raw.githubusercontent.com/tranhonghan/images/main/star_filled.png"
+		const starImgCorner = "https://raw.githubusercontent.com/tranhonghan/images/main/star_corner.png"
+		
+		return (
+			<View style={styles.customRatingBarStyle}>
+				{
+					maxRating.map((item, key)=>{
+						return (
+							<View
+								activeOpacity={0.7}
+								key= {item}
+							>
+								<Image
+									style={styles.starImgStyle} 
+									source={
+										item <= defaultRating
+											? {uri: starImgFilled}
+											: {uri: starImgCorner}
+									}
+								/>
+							</View>
+						)						
+					})
+				}
+			</View>
+		)
+	}
+
 	return(
-		<View style={{
+		<Pressable style={{
 			flexDirection: "row",
 			width: "100%", 
 			height: windowHeight*0.2, 
 			backgroundColor: "rgba(196, 196, 196, 0.1)",
 			marginBottom: 15
-		}}>
-			<Image style={{height: "100%", width: windowHeight*0.2, backgroundColor: "gray"}}/>
+		}} onPress={() => onPress()}>
+			<Image style={{height: "100%", width: windowHeight*0.2, backgroundColor: "gray"}} source={{uri: imageUrl}}/>
 			<View style={{flexDirection: "column", width: windowWidth-30-windowHeight*0.2, padding: 5}}>
 				<View style={{flexDirection: "row", justifyContent: "space-between", marginBottom: 2}}>
-					<Text>상호명</Text>
-					<Text>별점</Text>
+					<Text>{store}</Text>
+					<CustomRatingBar/>
 				</View>
 				<View style={{flexDirection: "row", alignItems: "center", marginBottom: 3}}>
 					<Location name="location-outline" size={14}/>
-					<Text>주소</Text>
+					<Text>{address}</Text>
 				</View>
 				<View>
-					<Text numberOfLines={5}>게시물 내용</Text>
+					<Text numberOfLines={5}>{contents}</Text>
 				</View>
 			</View>
-		</View>
+		</Pressable>
 	)
 }
+
+const styles = StyleSheet.create({
+	customRatingBarStyle : {
+		justifyContent: "flex-end",
+		flexDirection: "row",
+		marginTop: 3,
+	},
+	starImgStyle : {
+		padding: 5,
+		width: 12,
+		height: 12,
+		resizeMode: "cover"
+	},
+})
 
 export default RcmdPost
