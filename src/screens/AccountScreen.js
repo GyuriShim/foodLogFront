6950e3f7cc3c11deea31f62c9785b7ba3eb60ada
GyuriShim <rabitskr@naver.com>
2022-10-20 14:02:02 +0900

    feat: api 연결

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,9 +1,11 @@
-import React, {useLayoutEffect, useState} from "react"
+import React, {useEffect, useLayoutEffect, useState} from "react"
 import { View, StyleSheet, Image, Text, ScrollView, Pressable } from "react-native"
 import styled from "styled-components"
 import MapView from "react-native-maps"
 import PropTypes from "prop-types"
 import { getItemFromAsync } from "../utils/StorageFun"
+import { getMember } from "../service/member"
+import { follower,following } from "../service/subscribe"
 
 const Container = styled.View`
 	align-items: center
@@ -14,14 +16,54 @@ const Container = styled.View`
 	justify-content: space-around
 `
 
-const AccountScreen = ({navigation}) => {
+const AccountScreen = ({navigation, route}) => {
 	const [url, setUrl] = useState()
-	const [follower, setFollower] = useState()
-	const [following, setFolloing] = useState()
-	const [userName, setUserName] = useState("asdf")
+	const [userFollower, setUserFollower] = useState()
+	const [userFollowing, setUserFolloing] = useState()
+	const [userName, setUserName] = useState()
 	const [selfBio, setSelfBio] = useState()
+	const [loading, setLoading] = useState(false)
+	const [myId, setMyId] = useState()
+	const [memberId, setMemberId] = useState()
 
-	
+	const fetchProfile = async () => {
+		try{
+			setLoading(true)
+			var response
+			var followerRes
+			var followingRes
+			const userInfo = JSON.parse(await getItemFromAsync("user"))
+			setMyId(userInfo.id)
+			console.log("user",userInfo)
+			console.log("route",route)
+			if (route?.params) {
+				response = await getMember(route?.params)
+				followerRes = await follower(route?.params)
+				followingRes = await following(route?.params)
+			} else {
+				response = await getMember(userInfo.id)
+				followerRes = await follower(userInfo.id)
+				followingRes = await following(userInfo.id)
+			}
+			//const response = await getMember(route !== null? route?.params : userInfo.id)
+			console.log(followerRes.data, followingRes.data)
+			setMemberId(response.data.memberId)
+			setUserName(response.data.username)
+			setSelfBio(response.data.selfBio)
+			setUrl(response.data.profilePicture)
+			setUserFolloing(followingRes.data.totalElements)
+			setUserFollower(followerRes.data.totalElements)
+		} catch(e){
+			console.log("catch error", e)
+		}
+		setLoading(false)
+	}
+
+	useEffect(() => {
+		fetchProfile()
+	}, [])
+
+	if (loading) return <Text>로딩 중</Text>
 
 	return(
 		<>
@@ -46,14 +88,14 @@ const AccountScreen = ({navigation}) => {
 							}}
 						>
 							<Pressable style={{justifyContent: "center"}} onPress={() => navigation.navigate("follower")}>
-								<Text style={{alignSelf: "center"}}>{follower}</Text>
+								<Text style={{alignSelf: "center"}}>{userFollower}</Text>
 								<Text style={{alignSelf: "center"}}>팔로워</Text>
 							</Pressable>
 							<Pressable style={{justifyContent: "center"}} onPress={() => navigation.navigate("following")}>
-								<Text style={{alignSelf: "center"}}>{following}</Text>
+								<Text style={{alignSelf: "center"}}>{userFollowing}</Text>
 								<Text style={{alignSelf: "center"}}>팔로잉</Text>
 							</Pressable>
-							{userName==="asdf"? 
+							{myId===memberId? 
 								<Pressable style={{justifyContent: "center"}} onPress={() => navigation.navigate("Modify")}>
 									<Text>수정</Text>
 								</Pressable>
