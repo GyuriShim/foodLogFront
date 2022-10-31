da9bb404dd0f85193d0e2fc95323e4171322f5c9
jiwon1031 <yewchung56@naver.com>
2022-10-31 15:08:11 +0900

    feat:게시글사진 다중선택

--- src/screens/UploadScreen.js
+++ src/screens/UploadScreen.js
@@ -1,7 +1,6 @@
 import React,{useState, useEffect, useRef} from "react"
 import { launchImageLibrary } from "react-native-image-picker"
 import {ScrollView, Image, StatusBar, View, Text,  TouchableOpacity, Platform, TextInput, ActivityIndicator, StyleSheet} from "react-native"
-//import Pressable from "react-native/Libraries/Components/Pressable/Pressable"
 import {useRoute} from "@react-navigation/native"
 import axios from "axios"
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view"
@@ -9,10 +8,11 @@ import styled from "styled-components"
 import Button from "../components/Button"
 import DateTimePickerModal from "react-native-modal-datetime-picker"
 import { Picker } from "@react-native-picker/picker"
-//import RNPickerSelect from "react-native-picker-select"
 import ImagePicker from "react-native-image-picker"
 import { createPostApi } from "../service/post"
 import KeySearchScreen from "../screens/KeySearchScreen"
+import {placeSearch} from "../screens/KeySearchScreen"
+import { FlatList } from "react-native-gesture-handler"
 
 const Container = styled.View` 
   flex: 1
@@ -256,7 +256,7 @@ function UploadScreen({onChangeDate, navigation }){
 		const image = {
 			uri: "",
 			type: "",
-			name: "",
+			fileName: "",
 			timestamp:"",
 		}
 		await launchImageLibrary(
@@ -264,19 +264,53 @@ function UploadScreen({onChangeDate, navigation }){
 				mediaType: "photo",
 				includeBase64: Platform.OS === "android",
 				includeExtra: true,
+				selectionLimit: 5,
 			},
-			(res) => {
-				if(res.didCancel){
+			(response) => {
+				if(response.didCancel){
 					return
 				}
-				setResponse(res)
-				if(res?.assets[0]?.timestamp !== null){
-					var originDate = res?.assets[0]?.timestamp.split("T")
+				console.log(response.assets[0].uri)
+				setResponse(response)
+				if(response?.assets[0]?.timestamp !== null){
+					var originDate = response?.assets[0]?.timestamp.split("T")
 					setDate(originDate[0])
+					
 				}
-				
+				console.log(response.assets[0].fileName)
 				
 			}
+
+		)
+	}
+	const flatlistImage = () => {
+		return (
+			<View style={{width: 200, height:200, justifyContent: "center", alignItems: "center"}}>
+				<Image
+					source={{uri: response?.assets[0]?.uri}}
+					resizeMode="cover"
+				/>
+				<Image
+					source={{uri: response?.assets[1]?.uri}}
+					resizeMode="cover"
+				/>
+				<Image
+					source={{uri: response?.assets[2]?.uri}}
+					resizeMode="cover"
+				/>
+				<Image
+					source={{uri: response?.assets[3]?.uri}}
+					resizeMode="cover"
+				/>
+				<Image
+					source={{uri: response?.assets[4]?.uri}}
+					resizeMode="cover"
+				/>
+				<Image
+					source={{uri: response?.assets[5]?.uri}}
+					resizeMode="cover"
+				/>
+			</View>
 
 		)
 	}
@@ -325,13 +359,49 @@ function UploadScreen({onChangeDate, navigation }){
 			<Container >
 				<ScrollView>
 					<Box1>
+						<View style={{flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}>
+							<ScrollView
+								horizontal={true}>
+							
+								{/* <FlatList
+							data={{uri: response?.assets?.uri}}
+							horizontal="true"
+							renderItem={({item, index})=>{
+								return(
+									<flatlistImage item={item} index = {index}/>
+								)
+							}
+							}
+						/> */}
+								<Image
+									style={{width: 200, height:200, justifyContent: "center", alignItems: "center"}}
+									source={{uri: response?.assets[0]?.uri}}
+									resizeMode="cover"
+								/>
+								<Image
+									style={{width: 200, height:200, justifyContent: "center", alignItems: "center"}}
+									source={{uri: response?.assets[1]?.uri}}
+									resizeMode="cover"
+								/>
+								<Image
+									style={{width: 200, height:200, justifyContent: "center", alignItems: "center"}}
+									source={{uri: response?.assets[2]?.uri}}
+									resizeMode="cover"
+								/>
+								<Image
+									style={{width: 200, height:200, justifyContent: "center", alignItems: "center"}}
+									source={{uri: response?.assets[3]?.uri}}
+									resizeMode="cover"
+								/>
+								<Image
+									style={{width: 200, height:200, justifyContent: "center", alignItems: "center"}}
+									source={{uri: response?.assets[4]?.uri}}
+									resizeMode="cover"
+								/>
+							</ScrollView>
+						</View>
 						<View style={{flex:0, padding:1}}>
 							<Button title="이미지 업로드" color={"lightblue"} onPress={selectImage}></Button>
-							<Image
-								style={{width: 200, height:200, justifyContent: "center", alignItems: "center"}}
-								source={{uri: response?.assets[0]?.uri}}
-								resizeMode="cover"
-							/>
 						</View>
 					</Box1>
 					<View style = {styles.Box2}>
@@ -349,12 +419,12 @@ function UploadScreen({onChangeDate, navigation }){
 							onConfirm={handleConfirm}
 							onCancel={hideDatePicker}
 							is24Hour={true}
-							//display= "default"
-							//date={date}
+						//display= "default"
+						//date={date}
 						/>
 					</View>
 					<View style = {styles.Box2}>
-						<Text style = {{flex: 1}}>{"hi"}</Text>
+						<Text style = {{flex: 1}}>{"백소정"}</Text>
 						<Button style = {{ flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}
 							title="상호명" onPress={() => navigation.navigate("KeySearchScreen")}/>
 						<TouchableOpacity>
@@ -389,14 +459,6 @@ function UploadScreen({onChangeDate, navigation }){
 								selectedValue={purpose}
 								onValueChange={value =>  setPurpose(value)}
 								useNativeAndroidPickerStyle={false}
-								/* items={[
-									{ label: "친구", value: "FRIEND" },
-									{ label: "혼밥", value: "SOLO" },
-									{ label: "가족", value: "FAMILY" },
-									{ label: "회식", value: "MEETING" },
-									{ label: "데이트", value: "COUPLE" },
-									{ label: "기타", value: "ETC" },
-								]} */
 							>
 								<Picker.Item label="혼밥" value="SOLO" fontFamily="SF-Pro-Text-Semibold"/>
 								<Picker.Item label="데이트" value="COUPLE" fontFamily="SF-Pro-Text-Semibold"/>
